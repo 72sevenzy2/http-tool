4113f24752c8fb10e6d9dcc57af12c205048e367
72 <72sevenzy2@gmail.com>
2026-07-11 17:31:00 +0500

    refactor: remove redundant else statement to avoid further nesting.

--- session.go
+++ session.go
@@ -52,148 +52,147 @@ func StartSession(b *bufio.Scanner, store *Data) {
 			if len(parts) < 2 { // validate arguments before continuing (other it will panic)
 				fmt.Println("variable as second argument does not exit, consider setting a var.")
 				continue
-			} else {
-				val, err := store.Get(parts[1]) // check if parts[1] exists as a var first
-				if err != nil {
-					fmt.Println(err.Error())
-					continue
-				}
+			}
+			val, err := store.Get(parts[1]) // check if parts[1] exists as a var first
+			if err != nil {
+				fmt.Println(err.Error())
+				continue
+			}
 
-				//  utility variables
-				var (
-					pass    bool // determine whether final output block runs
-					reqType string
-					// bodyData map[string]string
-					jsonData string
-				)
-				pass = true
+			//  utility variables
+			var (
+				pass    bool // determine whether final output block runs
+				reqType string
+				// bodyData map[string]string
+				jsonData string
+			)
+			pass = true
 
-				// request dependent variables
-				var (
-					cl    *http.Request
-					clErr error
+			// request dependent variables
+			var (
+				cl    *http.Request
+				clErr error
 
-					// for appending headers
-					reqHeaders []string // reqHeaders being globally accessed by this scope, so headers can be assigned after initialising request.
+				// for appending headers
+				reqHeaders []string // reqHeaders being globally accessed by this scope, so headers can be assigned after initialising request.
 
-					formBody io.Reader // to hold form data
-				)
+				formBody io.Reader // to hold form data
+			)
 
-				// default request (GET) so cl does not stay nil if user were not to pass -X
-				cl, clErr = http.NewRequest(http.MethodGet, val, nil)
-				// clErr is handled below, after flags are parsed and appropriate requests are made.
+			// default request (GET) so cl does not stay nil if user were not to pass -X
+			cl, clErr = http.NewRequest(http.MethodGet, val, nil)
+			// clErr is handled below, after flags are parsed and appropriate requests are made.
 
-				// parse header arguments
-				for i := range parts {
-					upc := strings.ToUpper(parts[i]) // normalize "-h" to all uppercase
+			// parse header arguments
+			for i := range parts {
+				upc := strings.ToUpper(parts[i]) // normalize "-h" to all uppercase
 
-					if upc == "-H" {
-						// check if header values exist
-						if i+1 >= len(parts) {
-							fmt.Println("missing header values.")
-							pass = false
-							continue
-						}
-
-						reqHeaders = strings.SplitN(parts[i+1], ":", 2) // splits headers as:
-						// for example: header1:value, splitN() would split it so:
-						// map[string]string{
-						// 	"header1",
-						//  "value",
-						// }
-
-						if len(reqHeaders) < 2 || len(reqHeaders) > 2 { // validate length of headers or it will panic during execution
-							fmt.Println("please include both header name and value.")
-							pass = false
-							break
-						}
-						continue // skip
-
-						// refactored above ^ (keeping this block for future reference)
-						// if reqHeaders[0] == "" && reqHeaders[1] == "" {
-						// 	// cl.Header.Add(headers[0], headers[1])
-						// 	continue
-						// }
+				if upc == "-H" {
+					// check if header values exist
+					if i+1 >= len(parts) {
+						fmt.Println("missing header values.")
+						pass = false
+						continue
 					}
 
-					// to check if argument is for req methods
-					if upc == "-X" {
+					reqHeaders = strings.SplitN(parts[i+1], ":", 2) // splits headers as:
+					// for example: header1:value, splitN() would split it so:
+					// map[string]string{
+					// 	"header1",
+					//  "value",
+					// }
 
-						// validate if values exist (GET or POST)
-						if i+1 >= len(parts) {
-							fmt.Println("missing argument values, consider either POST or GET.")
+					if len(reqHeaders) < 2 || len(reqHeaders) > 2 { // validate length of headers or it will panic during execution
+						fmt.Println("please include both header name and value.")
+						pass = false
+						break
+					}
+					continue // skip
+
+					// refactored above ^ (keeping this block for future reference)
+					// if reqHeaders[0] == "" && reqHeaders[1] == "" {
+					// 	// cl.Header.Add(headers[0], headers[1])
+					// 	continue
+					// }
+				}
+
+				// to check if argument is for req methods
+				if upc == "-X" {
+
+					// validate if values exist (GET or POST)
+					if i+1 >= len(parts) {
+						fmt.Println("missing argument values, consider either POST or GET.")
+						pass = false
+						continue
+					}
+
+					newReq := strings.ToUpper(parts[i+1])
+
+					if newReq == "POST" {
+						reqType = http.MethodPost
+
+						// validate if request body flag also exists aswell as its data
+						if i+2 >= len(parts) {
+							fmt.Println("missing request body flag, consider: -d [data]")
 							pass = false
 							continue
 						}
 
-						newReq := strings.ToUpper(parts[i+1])
+						uppercased1 := strings.ToUpper(parts[i+2]) // normalize parts[i+2] to uppercase upon input
 
-						if newReq == "POST" {
-							reqType = http.MethodPost
+						if uppercased1 == "-D" { // for normal json data
 
-							// validate if request body flag also exists aswell as its data
-							if i+2 >= len(parts) {
-								fmt.Println("missing request body flag, consider: -d [data]")
+							// validate if values exist
+							if i+3 >= len(parts) {
+								fmt.Println("please include actual data in json format.")
 								pass = false
 								continue
 							}
 
-							uppercased1 := strings.ToUpper(parts[i+2]) // normalize parts[i+2] to uppercase upon input
+							// collect all input after parts[i+3]
+							// whilst also removing "\"
+							cleaned := strings.ReplaceAll(strings.ReplaceAll(strings.Join(parts[i+3:], ""), " ", ""), "\\", "")
 
-							if uppercased1 == "-D" { // for normal json data
+							// assign jsonData to cleaned
+							jsonData = cleaned
 
-								// validate if values exist
-								if i+3 >= len(parts) {
-									fmt.Println("please include actual data in json format.")
-									pass = false
-									continue
-								}
+						}
 
-								// collect all input after parts[i+3]
-								// whilst also removing "\"
-								cleaned := strings.ReplaceAll(strings.ReplaceAll(strings.Join(parts[i+3:], ""), " ", ""), "\\", "")
-
-								// assign jsonData to cleaned
-								jsonData = cleaned
-
-							}
-
-							if uppercased1 == "-F" { // form mode
-								if i+3 >= len(parts) {
-									fmt.Println("please include the necessary form data with format: 'title:value'")
-									pass = false
-									continue
-								}
-
-								formParts := strings.SplitN(parts[i+3], ":", 2) // needs to be in a format like so:
-								// "title:value", and strings.SplitN(...) would return:
-								// map[string]string{
-								//		"title": "value",
-								// }
-
-								// validate if formParts is of correct length now (or will panic)
-								if len(formParts) < 2 || len(formParts) > 2 {
-									fmt.Println("please use the correct format.")
-									pass = false
-									continue
-								}
-
-								// appending the form values
-								data := url.Values{}
-
-								data.Set(formParts[0], formParts[1])
-								enc := data.Encode() // encode
-
-								formBody = strings.NewReader(enc)
-							}
-
-						} else {
-							if newReq == "GET" {
-								reqType = http.MethodGet
-							} else {
-								fmt.Println("invalid method type.")
+						if uppercased1 == "-F" { // form mode
+							if i+3 >= len(parts) {
+								fmt.Println("please include the necessary form data with format: 'title:value'")
+								pass = false
 								continue
 							}
+
+							formParts := strings.SplitN(parts[i+3], ":", 2) // needs to be in a format like so:
+							// "title:value", and strings.SplitN(...) would return:
+							// map[string]string{
+							//		"title": "value",
+							// }
+
+							// validate if formParts is of correct length now (or will panic)
+							if len(formParts) < 2 || len(formParts) > 2 {
+								fmt.Println("please use the correct format.")
+								pass = false
+								continue
+							}
+
+							// appending the form values
+							data := url.Values{}
+
+							data.Set(formParts[0], formParts[1])
+							enc := data.Encode() // encode
+
+							formBody = strings.NewReader(enc)
+						}
+
+					} else {
+						if newReq == "GET" {
+							reqType = http.MethodGet
+						} else {
+							fmt.Println("invalid method type.")
+							continue
 						}
 					}
 
