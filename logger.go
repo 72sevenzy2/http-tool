7fd61a05efba816271aa43ddb6a7fb2e633f6ccb
72 <72sevenzy2@gmail.com>
2026-07-29 17:11:12 +0500

    refactor/feat: add concurrent testing compatability (and changed logging structure).

--- logger.go
+++ logger.go
@@ -9,36 +9,51 @@ import (
 )
 
 // return config
+type RequestResult struct {
+	ReqResponse *http.Response
+	Reqtime     time.Duration
+	Reqbody     string
+	Reqlen      int
+	Reqerr      error
+}
+
 type LoggerConf struct {
-	Req     *http.Response
-	Reqtime time.Duration
-	Reqbody string
-	Reqlen  *int
-	Reqerr  error
+	Results []*RequestResult
 }
 
 // small utils for LoggerConf
 
-func (b LoggerConf) DisplayReqStatus() string {
-	return b.Req.Status
+func (b LoggerConf) DisplayReqStatus() {
+	for v := range len(b.Results) {
+		fmt.Println(b.Results[v].ReqResponse.Status)
+	}
 }
 
 func (b LoggerConf) DisplayReqLatency() {
-	fmt.Println("request latency:", b.Reqtime)
+	for v := range len(b.Results) {
+		fmt.Println("request latency:", b.Results[v].Reqtime)
+	}
 }
 
 func (b LoggerConf) DisplayReqBodyLen() {
-	fmt.Println("request body length:", b.Reqlen)
+	for v := range len(b.Results) {
+		fmt.Println("request body length:", b.Results[v].Reqlen)
+	}
 }
 
 func (b LoggerConf) DisplayReqBody() {
-	fmt.Println(b.Reqbody)
+	for v := range len(b.Results) {
+		fmt.Println(b.Results[v].Reqbody)
+	}
 }
 
 func (b LoggerConf) Err() {
-	fmt.Println(b.Reqerr.Error())
+	for _, v := range b.Results {
+		if v.Reqerr != nil {
+			fmt.Println(v.Reqerr)
+		}
+	}
 }
-
 
 // for params
 type LoggerParamConf struct {
@@ -46,27 +61,44 @@ type LoggerParamConf struct {
 	Req          *http.Request
 	AllowReqBody *bool
 	ReqBodySize  *int
+	ReqNo        *int
 }
 
-func Log(conf *LoggerParamConf) *LoggerConf {
-	start := time.Now()
-	resp, err := conf.Reqclient.Do(conf.Req)
-	if err != nil {
-		return &LoggerConf{
-			Req:     nil,
-			Reqtime: 0,
-			Reqbody: "",
-			Reqlen:  IntPtr(0),
-			Reqerr:  err,
-	 	}
-	}
-	end := time.Since(start)
+func Log(conf *LoggerParamConf) *LoggerConf { // move time.Now() (start) and end to inside gorountines.
+	resultChan := make(chan *RequestResult, *conf.ReqNo)
+	// ^ channels for number of workers per request
+	for  range *conf.ReqNo {
+		go func(conf *LoggerParamConf, result chan<- *RequestResult) {
+			start := time.Now()
+			resp, resperr := conf.Reqclient.Do(conf.Req)
+			if resperr != nil {
+				result <- &RequestResult{
+					Reqerr: resperr,
+				}
+				return // exit if err (other will panic when touching resp.Body)
+			}
+			end := time.Since(start)
 
-	// fmt.Println("visited to:", req.URL.Path)
-	// fmt.Println("method:", req.Method)
+			result <- &RequestResult{
+				ReqResponse: resp,
+				Reqtime:     end,
+				Reqbody:     "",
+				Reqlen:      0,
+				Reqerr:      nil,
+			}
+
+		}(conf, resultChan)
+	}
+
+	var requests []*RequestResult
+
+	for range *conf.ReqNo {
+		res := <-resultChan
+		requests = append(requests, res)
+	}
+
 	fmt.Printf("visited to %s, with method %s", conf.Req.URL.Path, conf.Req.Method)
 	fmt.Println(" full url:", conf.Req.URL)
-	// request query
 	if conf.Req.URL.RawQuery != "" {
 		fmt.Println("with query:", conf.Req.URL.RawQuery)
 	}
@@ -78,35 +110,39 @@ func Log(conf *LoggerParamConf) *LoggerConf {
 	fmt.Println(newHeaders) // then display
 
 	// request body printing
-	var bodyprev string  // body preview (string) var
-	var max *int         // body size var
-	var bodybytes []byte // body size (in bytes)
+	for v := range len(requests) {
+		var bodyprev string // body preview (string) var
+		var max *int
+		var bodybytes []byte
+		var bodyerr error
 
-	if *conf.AllowReqBody {
-		max = conf.ReqBodySize
-		bodybytes, err = io.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println(err.Error())
+		if *conf.AllowReqBody {
+			max = conf.ReqBodySize
+			if requests[v].Reqerr != nil {
+				continue
+			}
+			bodybytes, bodyerr = io.ReadAll(requests[v].ReqResponse.Body)
+			if bodyerr != nil {
+				fmt.Println(bodyerr.Error())
+			}
+
+			requests[v].ReqResponse.Body = io.NopCloser(bytes.NewBuffer(bodybytes))
+
+			fmt.Println("max:", *max, "len:", len(bodybytes))
+			if *max > 0 && len(bodybytes) > *max {
+				bodybytes = bodybytes[:*max]
+			}
+			fmt.Println("after truncating:", len(bodybytes))
+			bodyprev = string(bodybytes)
+
 		}
 
-		resp.Body = io.NopCloser(bytes.NewBuffer(bodybytes))
+		final := len(bodybytes)
 
-		fmt.Println("max:", *max, "len:", len(bodybytes))
-		if *max > 0 && len(bodybytes) > *max {
-			bodybytes = bodybytes[:*max]
-		}
-		fmt.Println("after truncating:", len(bodybytes))
-		bodyprev = string(bodybytes)
-
+		requests[v].Reqlen = final
+		requests[v].Reqbody = bodyprev
 	}
-
-	final := len(bodybytes)
-
 	return &LoggerConf{
-		Req:     resp,
-		Reqtime: end,
-		Reqbody: bodyprev,
-		Reqlen:  &final,
-		Reqerr:  nil,
+		Results: requests,
 	}
 }
